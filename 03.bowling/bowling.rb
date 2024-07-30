99b4b5c858e10706709e375f58240002f4df0954
takao0206 <takao.haruna.0206@gmail.com>
2024-07-30 19:24:33 +0900

    指摘事項の修正（5点）
    1. 不要なコメントの削除
    2. スペアのスコア計算をより厳密にした（1フレーム目だけで10点の場合を除くようにした）
    3. `each_with_index`を用いて、不要なコードを削除した
    4. フレームのスコア計算で、メソッド化が過剰なところを見直し、コードを読みやすくした
    5. 不要な改行コード`\`を削除

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,11 +1,9 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# ボウリングのスコアを取得し、配列にする
 score = ARGV[0]
 scores = score.split(',')
 
-# ストライクを10点に変換する。ダミーの2投目を追加する。
 shots = []
 scores.each do |s|
   if s == 'X'
@@ -16,30 +14,21 @@ scores.each do |s|
   end
 end
 
-# 1フレームごとに束ねる
+# ボウリングでは、1～9ゲームまで、1フレームで2ショット投げられるので、フレームでまとめる。
 frames = shots.each_slice(2).to_a
 
-# ストライクかどうかの判定
 def strike?(frame)
   frame[0] == 10
 end
 
-# スペアかどうかの判定
 def spare?(frame)
-  frame[0] + frame[1] == 10
+  !strike?(frame) && frame[0] + frame[1] == 10
 end
 
-# 各フレームのスコアの計算
-def frame_score(frame)
-  frame.sum
-end
-
-# スペアの時のスコアの計算
 def spare_score(frames, frame_index)
   [frames[frame_index][0], frames[frame_index][1], frames[frame_index + 1][0]].sum
 end
 
-# ストライクの時のスコアの計算
 def strike_score(frames, frame_index)
   if frames[frame_index + 1][0] == 10
     [frames[frame_index][0], frames[frame_index + 1][0], frames[frame_index + 2][0]].sum
@@ -48,24 +37,19 @@ def strike_score(frames, frame_index)
   end
 end
 
-# 合計スコアを計算する
-frame_index = 0
 total_score = 0
-frames.each do |frame|
-  total_score += \
-    # 1～9フレームのスコア計算
+frames.each_with_index do |frame, frame_index|
+  total_score +=
     if frame_index < 9
       if strike?(frame)
         strike_score(frames, frame_index)
       elsif spare?(frame)
         spare_score(frames, frame_index)
       else
-        frame_score(frame)
+        frame.sum
       end
-    # 10フレームのスコア計算
     else
-      frame_score(frame)
+      frame.sum
     end
-  frame_index += 1
 end
 puts total_score
