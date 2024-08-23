037b0af84cd292e51be852b994267a4e5c55e289
takao0206 <takao.haruna.0206@gmail.com>
2024-08-23 09:59:07 +0900

    指摘事項の修正（5点）
    1. 不要なコメントの削除した。
    1. コマンドライン引数を取得する機能を削除し、カレントディレクトリのエントリだけを取得する機能に変更した。
    1. ディレクトリを表示する時、語尾に`/`をつけていたが、その機能を削除し、OSの`ls`と同じ出力表示にした。
    1. 複数のディレクトリのエントリを取得する機能を削除し、カレントディレクトリのエントリだけを取得する機能に変更した。
    1. マトリクスで出力する機能を`divmod`を用いて修正した。

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -7,56 +7,10 @@ require 'optparse'
 COLUMNS = 3
 
 def main
-  arguments = fetch_arguments
+  entries = Dir.entries(Dir.pwd).sort!
+  entries.reject! { |entry| entry.start_with?('.') }
 
-  # コマンドライン引数のパスが2つ以上ある場合、
-  # まずファイルをアルファベット順に並べ、次にディレクトリをその順に並べて、表示する。
-  arguments[:paths].sort_by! { |path| [File.file?(path) ? 0 : 1, path.downcase] }.each do |path|
-    entries = search_entries(path)
-    next if entries.nil?
-
-    entries.reject! { |entry| entry.start_with?('.') }
-    entries.sort!.map { |entry| File.directory?(File.join(path, entry)) ? entry << '/' : entry }
-
-    display_output(entries)
-  end
-end
-
-# コマンドライン引数を取得する。想定外の引数が入力された場合、エラーを返す。
-#
-# @return [Hash] 取得した引数がハッシュの形式で出力される。
-def fetch_arguments
-  arguments = { paths: [] }
-
-  # TODO: 今後のプラクティスで、オプション-a, -r, -lを以下に追加する予定。
-  opts = OptionParser.new
-
-  begin
-    opts.parse!(ARGV)
-  rescue OptionParser::InvalidOption => e
-    puts e.message
-    exit 2
-  end
-
-  arguments[:paths] = ARGV.empty? ? [Dir.pwd] : ARGV
-
-  arguments
-end
-
-# 指定されたパスのファイルエントリを検索する。
-#
-# @param path [String] ファイルエントリを検索したいパス
-# @return [Array] ファイルエントリを配列で返す。
-# @return 指定されたパスがファイルだった時は、ファイルのフルパスを返す。
-# @return 指定されたパスが見つからなかった時やアクセス不可の時は、nilを返す。
-def search_entries(path)
-  Dir.entries(path)
-rescue Errno::ENOENT
-  puts "cannot access '#{path}': No such file or directory"
-rescue Errno::EACCES
-  puts "cannot open '#{path}': Permission denied"
-rescue Errno::ENOTDIR
-  [path]
+  display_output(entries)
 end
 
 # 列幅を整えたファイルエントリをマトリクスで表示する。
@@ -69,8 +23,8 @@ def display_output(entries)
   output_matrix = Array.new(rows) { [] }
 
   entries.each_with_index do |entry, index|
-    row = index % rows
-    col = index / rows
+    row = index.divmod(rows)[1]
+    col = index.divmod(rows)[0]
     output_matrix[row][col] = entry.ljust(max_col_length)
   end
 
