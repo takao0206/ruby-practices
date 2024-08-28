94419098f159407903ed53c739da73c8c4aded19
takao0206 <takao.haruna.0206@gmail.com>
2024-08-28 11:11:52 +0900

    コマンドラインから指定されたオプション(`-a`)を解析し、取得する機能を追加。

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -8,9 +8,32 @@ COLUMNS = 3
 
 def main
   entries = Dir.entries(Dir.pwd).sort
-  entries.reject! { |entry| entry.start_with?('.') }
+  entries.reject! { |entry| entry.start_with?('.') } unless parse_command_line_options[:a]
 
   display_output(entries)
+end
+
+# コマンドラインから指定されたオプションを解析し、取得する。
+#
+# @return [Hash] 解析されたオプションがキーと値のペアでハッシュとして返される。
+#                例えば、`-a` オプションが指定された場合、 `{ a: true }` が返される。
+#                オプションが指定されない場合、デフォルトで `{ a: false }` が返される。
+#                また、無効なオプションが指定された場合は、エラーメッセージを表示し、プログラムを終了する。
+def parse_command_line_options
+  options = { a: false }
+
+  opts = OptionParser.new
+  opts.on('-a', 'do not ignore entries starting with .') { options[:a] = true }
+
+  begin
+    opts.parse(ARGV)
+  rescue OptionParser::InvalidOption
+    puts '指定されたオプションは無効です。以下、有効なオプションです。'
+    puts '  -a   .から始まるエントリも表示できます。'
+    exit 1
+  end
+
+  options
 end
 
 # 列幅を整えたファイルエントリをマトリクスで表示する。
